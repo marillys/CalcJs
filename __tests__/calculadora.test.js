0326d6ba9c8e45030c629e8dac9b29851b8e00e0
Marilia <marilia.borges01@outlook.com>
2022-11-03 21:04:14 -0300

    Incluir testes unitários para multiplicação, revisão e usando array como massa de teste

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -20,4 +20,44 @@ test('subtrair 50 - 10', () => {
     const resultadoObtido = subtrairDoisNumeros(num1,num2);
 
     expect(resultadoObtido).toBe(resultadoEsperado)
+});
+
+test('Multiplicar 6 * 3', () => {
+    const num1 = 6;
+    const num2 = 3;
+    const resultadoEsperado = 18;
+
+    const multiplicarDoisNumeros = calculadora.multiplicarDoisNumeros
+    
+    expect(multiplicarDoisNumeros(num1,num2)).toBe(resultadoEsperado)
+});
+
+test('Dividir 8 / 4', () => {
+    const num1=8;
+    const num2 = 4;
+    const resultadoEsperado = 2 
+    const dividirDoisNumeros = calculadora.dividirDoisNumeros;
+    expect(dividirDoisNumeros(num1,num2)).toBe(resultadoEsperado)
+});
+
+//Teste de exceção
+test('Dividir por zero', () => {
+    const num1 = 7;
+    const num2 = 0;
+    const resultadoEsperado = Infinity;
+    const dividirDoisNumeros = calculadora.dividirDoisNumeros;
+
+    expect(dividirDoisNumeros(num1,num2)).toBe(resultadoEsperado);
+});
+
+//Data Driven Test para a função dividirDoisNumeros
+const massaDivisao = [
+    [8,4,2],
+    [7,0,Infinity]
+]
+
+//Script que usa a massa de teste
+test.each(massaDivisao)('Dividir %f / %f', (num1, num2, resultadoEsperado) => {
+    const dividirDoisNumeros = calculadora.dividirDoisNumeros;
+    expect(dividirDoisNumeros(num1,num2)).toBe(resultadoEsperado);
 })
